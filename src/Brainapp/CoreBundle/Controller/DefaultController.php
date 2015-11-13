61e807428eaf8237031f2b98ccb308c8cf88724e
Michael Müller <development@reu-network.de>
2015-11-13 19:04:24 +0100

    added kernel.css and kernel.js to base template

--- src/Brainapp/CoreBundle/Controller/DefaultController.php
+++ src/Brainapp/CoreBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('BrainappTemplateBundle:Default:index.html.twig');
+        return $this->render('BrainappCoreBundle:Default:index.html.twig');
     }
 }
