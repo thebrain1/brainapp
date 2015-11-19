c86774400d72df33fa5b1927757eaa1bced79af5
Michael Müller <development@reu-network.de>
2015-11-19 22:10:00 +0100

    test fontawesome link

--- refresh_prod_env.sh
+++ refresh_prod_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 php app/console assets:install --env=prod --symlink
+php app/console assetic:dump --env=prod --no-debug
 php app/console cache:clear --env=prod
 php app/console cache:warmup --env=prod
-php app/console assetic:dump --env=prod --no-debug
 
 
