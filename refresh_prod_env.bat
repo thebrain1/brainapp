e73c2ad6c9ecc96e99555a8e2b3eb0b77ea45b91
Michael Müller <development@reu-network.de>
2015-11-20 14:39:30 +0100

    added prod refresh bash-script for windows

---
+++ refresh_prod_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+rem # refresh_prod_env.bat
+rem # 
+rem # Aktualisiert die Produktivumgebung des Webservers
+rem #   - installiert und dumpt Assets 
+rem #   - Cache wird aktualisiert
+
+php app/console assets:install --env=prod --symlink
+php app/console cache:clear --env=prod
+php app/console cache:warmup --env=prod
+php app/console assetic:dump --env=prod --no-debug
