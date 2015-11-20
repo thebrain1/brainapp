e4e0c5211c237e7947969d245c05f909a3ddf353
Michael Müller <development@reu-network.de>
2015-11-20 20:34:47 +0100

    redirect to dashboard, if user is authenticated

--- src/Brainapp/CoreBundle/Controller/DefaultController.php
+++ src/Brainapp/CoreBundle/Controller/DefaultController.php
@@ -22,6 +22,12 @@ class DefaultController extends Controller
 	 */
     public function indexAction()
     {
+    	// Weiterleitung auf Dashboard, wenn Benutzer bereits angemeldet
+    	$securityContext = $this->container->get('security.authorization_checker');
+    	if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY'))
+    		return $this->redirect($this->generateUrl('dashboard_home'));
+    	
+    	// sonst: erstelle Login
     	if ($this->has('security.csrf.token_manager')) 
     	{
     		$csrfToken = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
@@ -29,8 +35,7 @@ class DefaultController extends Controller
     	else 
     	{
     		// BC for SF < 2.4
-    		$csrfToken = $this->has('form.csrf_provider')
-    		? $this->get('form.csrf_provider')->generateCsrfToken('authenticate')
+    		$csrfToken = $this->has('form.csrf_provider') ? $this->get('form.csrf_provider')->generateCsrfToken('authenticate')
     		: null;
     	}
     	
