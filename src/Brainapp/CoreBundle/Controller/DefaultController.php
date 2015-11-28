1b64f6e8b50cbd6d21643c8585aeb0d72500a396
Michael Müller <development@reu-network.de>
2015-11-28 12:10:52 +0100

    testing login form

--- src/Brainapp/CoreBundle/Controller/DefaultController.php
+++ src/Brainapp/CoreBundle/Controller/DefaultController.php
@@ -7,6 +7,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Brainapp\UserBundle\Form\RegistrationFormType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * DefaultController
@@ -56,9 +58,50 @@ class DefaultController extends Controller
     		$userManager->updateUser($user);
     	}
     	
-        return array(
+    	// LOGIN
+    	$session = $request->getSession();
+    	
+    	if (class_exists('\Symfony\Component\Security\Core\Security')) {
+    		$authErrorKey = Security::AUTHENTICATION_ERROR;
+    		$lastUsernameKey = Security::LAST_USERNAME;
+    	} else {
+    		// BC for SF < 2.6
+    		$authErrorKey = SecurityContextInterface::AUTHENTICATION_ERROR;
+    		$lastUsernameKey = SecurityContextInterface::LAST_USERNAME;
+    	}
+    	
+    	// get the error if any (works with forward and redirect -- see below)
+    	if ($request->attributes->has($authErrorKey)) {
+    		$error = $request->attributes->get($authErrorKey);
+    	} elseif (null !== $session && $session->has($authErrorKey)) {
+    		$error = $session->get($authErrorKey);
+    		$session->remove($authErrorKey);
+    	} else {
+    		$error = null;
+    	}
+    	
+    	if (!$error instanceof AuthenticationException) {
+    		$error = null; // The value does not come from the security component.
+    	}
+    	
+    	// last username entered by the user
+    	$lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
+    	
+    	if ($this->has('security.csrf.token_manager')) {
+    		$csrfTokenLogin = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
+    	} else {
+    		// BC for SF < 2.4
+    		$csrfTokenLogin = $this->has('form.csrf_provider')
+    		? $this->get('form.csrf_provider')->generateCsrfToken('authenticate')
+    		: null;
+    	}
+    	
+    	return array(
+    			'last_username' => $lastUsername,
+    			'error' => $error,
+    			'csrf_token_login' => $csrfTokenLogin,
         		'form_registration' => $form->createView(),
-        		'csrf_token' => $csrfToken
+        		'csrf_token_registration' => $csrfToken
         );
     }
 }
