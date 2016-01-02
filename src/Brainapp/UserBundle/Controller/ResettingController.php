f320de697ade019c3814ee7ddfdea20ba0734c3c
Michael Müller <development@reu-network.de>
2016-01-02 16:49:02 +0100

    added template to controller

--- src/Brainapp/UserBundle/Controller/ResettingController.php
+++ src/Brainapp/UserBundle/Controller/ResettingController.php
@@ -7,12 +7,17 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\UserBundle\Controller\ResettingController as BaseController;
 
+/**
+ * @author Michael Müller <development@reu-network.de>
+ * 
+ * @Route("/resetting")
+ */
 class ResettingController extends BaseController
 {
 	/**
 	 * Request reset user password: show form
 	 * @Route("/request", name="resetting_request")
-	 * @Template()
+	 * @Template("BrainappUserBundle:Resetting:request.html.twig")
 	 */
 	public function requestAction()
 	{
