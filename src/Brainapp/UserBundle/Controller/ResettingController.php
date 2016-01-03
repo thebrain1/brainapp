e12984780264233542bba29d0a4557ef94b89714
Michael Müller <development@reu-network.de>
2016-01-03 15:34:39 +0100

    * improved resetting modal

--- src/Brainapp/UserBundle/Controller/ResettingController.php
+++ src/Brainapp/UserBundle/Controller/ResettingController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use FOS\UserBundle\Controller\ResettingController as BaseController;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * @author Michael Müller <development@reu-network.de>
@@ -22,5 +25,55 @@ class ResettingController extends BaseController
 	public function requestAction()
 	{
 		return array();
+	}
+	
+	/**
+	 * Overrides FOSUser sendEmailAction
+	 * @Route("/send-email", name="resetting_send_email")
+	 */
+	public function sendEmailAction(Request $request)
+	{
+	    $username = $request->request->get('username');
+	    $email = $request->request->get('email');
+	    
+	    /** @var $user UserInterface */
+	    $user = $this->get('fos_user.user_manager')->findUserByUsernameOrEmail($username);
+	
+	    // Kein Benutzer gefunden
+	    if (null === $user) 
+	    {
+	        return new JsonResponse(array(
+	            'error' => true,
+	            'message' => "Der Benutzername oder die E-Mail-Adresse $username existiert nicht."), 200);
+	    }
+	    // falsche E-Mail
+	    elseif($email !== $user->getEmail())
+	    {
+	        return new JsonResponse(array(
+	            'error' => true,
+	            'message' => "Der Benutzername und die E-Mail-Adresse können einander nicht zugeordnet werden."), 200);
+	    }
+	    // Passwortruecksetzung bereits beantragt
+	    elseif ($user->isPasswordRequestNonExpired($this->container->getParameter('fos_user.resetting.token_ttl'))) 
+	    {
+	        return new JsonResponse(array(
+	            'error' => true,
+	            'message' => "Für diesen Benutzer wurde in den vergangenen 24 Stunden bereits ein neues Passwort beantragt."), 200);
+	    }
+	    if (null === $user->getConfirmationToken()) 
+	    {
+	        /** @var $tokenGenerator \FOS\UserBundle\Util\TokenGeneratorInterface */
+	        $tokenGenerator = $this->get('fos_user.util.token_generator');
+	        $user->setConfirmationToken($tokenGenerator->generateToken());
+	    }
+	
+	    $this->get('fos_user.mailer')->sendResettingEmailMessage($user);
+	    $user->setPasswordRequestedAt(new \DateTime());
+	    $this->get('fos_user.user_manager')->updateUser($user);
+	    
+	    $email =  $this->getObfuscatedEmail($user);
+	    return new JsonResponse(array(
+	        'success' => true, 
+	        'message' => "Eine E-Mail wurde an $email gesendet. Sie enthält einen Link, den Sie anklicken müssen, um Ihr Passwort zurückzusetzen."), 200);
 	}
 }
--- src/Brainapp/UserBundle/Resources/public/js/modal_login.js
+++ src/Brainapp/UserBundle/Resources/public/js/modal_login.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
 	$('button.close').click(function(e) {
 		e.preventDefault();
-		$('#error').slideUp(300);
+		$(this).parent('#error').slideUp(300);
+//		$('#loginModal #error').slideUp(300);
+//		$('#resettingModal #error').slideUp(300);
 	});
 	
 	$('#resettingModal').on('shown.bs.modal', function () {
@@ -14,10 +16,9 @@ $(document).ready(function() {
 	})
 	
 	/* HANDLE AJAX LOGIN */
-	$('.modal #submit').click(function(e) {
+	$('.modal #form-login #submit').click(function(e) {
 		e.preventDefault();
 		var $form = $("#form-login");
-		$('#username').select();
 		$.ajax({
 			type : $form.attr('method'),
 			url : $form.attr('action'),
@@ -32,6 +33,35 @@ $(document).ready(function() {
 				}
 				else
 					location.reload();
+			},
+			error : function(data, status, object) {
+				console.log(data);
+				alert("Es ist ein unbekannter Fehler aufgetreten!")
+			}
+		});
+	});
+	/* HANDLE AJAX PASSWORT RESET */
+	$('.modal #form-resetting #submit').click(function(e) {
+		e.preventDefault();
+		var $form = $("#form-resetting");
+		$.ajax({
+			type : $form.attr('method'),
+			url : $form.attr('action'),
+			data : $form.serialize(),
+			dataType : "json",
+			success : function(data, status, object) 
+			{
+				if (data.error)
+				{
+					$('#resettingModal #error #message').html(data.message);
+					$('#resettingModal #error').slideDown(300);
+				}
+				else
+				{
+					$form.hide();
+					$('#resettingModal #success #message').html(data.message);
+					$('#resettingModal #success').slideDown(300);
+				}
 			},
 			error : function(data, status, object) {
 				console.log(data);
